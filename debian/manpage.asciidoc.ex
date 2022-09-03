05a33284409253a23e7116f38b14c2c8b2a25440
Vítězslav Dvořák <info@vitexsoftware.cz>
2022-09-03 15:26:04 +0200

    Initial packaging

---
+++ debian/manpage.asciidoc.ex
@@ -0,0 +1,41 @@
+= @UPACKAGE@(1)
+:man source:   PYTHON3-TAPOP100
+:man version:  0.1
+:man manual:   PYTHON3-TAPOP100 Manual
+
+// See "Manpage Documents" in "AsciiDoc User Guide"
+// http://www.methods.co.nz/asciidoc/userguide.html
+
+// convert by:
+// $ a2x --doctype manpage --format manpage python3-tapop100.asciidoc
+
+== NAME
+
+python3-tapop100 - program to do something
+
+== SYNOPSIS
+
+*python3-tapop100*  [*-h*] ... 
+
+
+== DESCRIPTION
+
+*python3-tapop100* do something.
+
+=== optional arguments:
+
+*-h*, *--help*::
+    show this help message and exit.
+
+
+== AUTHOR
+
+Copyright © 2022 Vítězslav Dvořák <info@vitexsoftware.cz>
+
+== LICENSE
+
+MIT License
+
+== SEE ALSO
+
+See also ...
